fbada6cac1e44a894f844554267e49501f820537
Evan Stone <evan@cloudcity.io>
2016-08-12 16:00:51 -0700

    Initial Add

---
+++ BLEConnect/BLEConnect/ViewController/ManagerSelectionViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ManagerSelectionViewController.swift
+//  BLEConnect
+//
+//  Created by Evan Stone on 8/12/16.
+//  Copyright © 2016 Cloud City. All rights reserved.
+//
+
+import UIKit
+
+class ManagerSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func handleCentralButtonTapped(sender: AnyObject) {
+        self.performSegueWithIdentifier("showCentralManagerViewController", sender: self)
+    }
+    
+    @IBAction func handlePeripheralButtonTapped(sender: AnyObject) {
+        self.performSegueWithIdentifier("showPeripheralManagerViewController", sender: self)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
